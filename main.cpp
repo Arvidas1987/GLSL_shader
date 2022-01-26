8ebf5b494589ad902d35130345daaf012314efe7
Marcin Damian Roszkowski <space20@wp.pl>
2022-01-26 23:21:14 +0100

    Created compile_shaders() function to draw point.

--- main.cpp
+++ main.cpp
@@ -28,16 +28,70 @@ if "undefined reference..."
 #define NEWLN() std::cout << "\n";
 #
 
+
+GLuint compile_shaders()
+{
+static const char * vs_source[] ={
+    R"glsl(
+        "#version 420 core
+            void main(void){
+                gl_Position = vec4(0.0, 0.0, 0.5, 1.0);
+            }
+        )glsl"
+};
+
+static const char * fs_source[] = {
+    R"glsl(
+        #version 420 core
+        out vec4 color;
+        void main(void){
+            color = vec4(0.0, 0.8, 1.0, 1.0);
+        }
+    )glsl"
+};
+
+//utworzenie i kompilacja shadera wiercholkow
+GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+glShaderSource(vertex_shader, 1, vs_source, nullptr);
+glCompileShader(vertex_shader);
+
+//utworzenie i kompilacja shadera fragmentow
+GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+glShaderSource(fragment_shader, 1, fs_source, nullptr);
+glCompileShader(fragment_shader);
+
+//utworzenie programu, dodanie shaderow i ich polaczenie
+GLuint program = glCreateProgram();
+glAttachShader(program, vertex_shader);
+glAttachShader(program, fragment_shader);
+glLinkProgram(program);
+
+//usuniecie shaderow, bo znajduja sie juz w programie
+glDeleteShader(vertex_shader);
+glDeleteShader(fragment_shader);
+
+return program;
+}
+
+
 int main()
 {
-    GLFWwindow* window;
 
     /* Initialize the library */
-    if ( glfwInit() == GLFW_FALSE )
+    if ( glfwInit() == GLFW_FALSE ){
+        WRITELN( "[PL] Problem z inicjalizacja GLFW." )
+        WRITELN( "[EN] GLFW initialization issue." )
+        //WRITELN( "\t|-> " << glewGetErrorString(GLEW_init_error) )NEWLN()
         return -1;
+    } else {
+        WRITELN( "[PL] Inicjalizacja GLFW:  OK" );
+        WRITELN( "[EN] GLFW inicjalization: OK" );
+        NEWLN()
+        //return 0;
+    }
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow( 640, 480, "GLFW_64bits", NULL, NULL );
+    GLFWwindow* window = glfwCreateWindow( 640, 480, "GLFW_64bits", NULL, NULL );
     if ( window == nullptr )
     {
         glfwTerminate();
@@ -67,12 +121,22 @@ int main()
         //return 0;
     }
 
+
+
+
+GLuint programm = compile_shaders();
+
     /* Loop until the user closes the window */
     while ( !glfwWindowShouldClose(window) )
     {
         /* Render here */
         glClearColor( 0.5f, 0.5f, 0.5f, 1.0f );
         glClear( GL_COLOR_BUFFER_BIT );
+
+        glUseProgram(programm);
+        glPointSize(40.0f);
+        glDrawArrays(GL_POINTS, 0, 1);
+
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
